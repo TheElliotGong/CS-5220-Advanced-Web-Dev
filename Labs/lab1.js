69358030aad0c38ba15027f849a06968016d5d7f
thetyper101 <ewcgong@hotmail.com>
2025-09-12 10:17:54 -0700

    Added all 5 functions of lab 1 and removed debug settings file

--- Labs/lab1.js
+++ Labs/lab1.js
@@ -14,8 +14,7 @@ console.log(letterMap(['iPhone', 'Galaxy', 'Pixel', 'Nokia'])
 function numberPalindrome(x)
 {
     if (typeof x !== 'number') {
-        console.log('Input is not a number');
-        return null;
+        return 'Input is not a number';
     }
     let obj = {};
     obj['number'] = x;
@@ -24,6 +23,7 @@ function numberPalindrome(x)
     
     return obj;
 }
+console.log(numberPalindrome("hello"));
 console.log(numberPalindrome(54145));
 console.log(numberPalindrome(13475));
 
@@ -42,3 +42,53 @@ function groupTypes(array)
     return obj;
 }
 console.log(JSON.stringify(groupTypes([1, 'hello', true, 3.14, 'world', false, {"name": "viking", "weapon": "axe"}, 42])));
+
+function weaveArrays(array1, array2)
+{
+    if(array1.length !== array2.length)
+    {
+        return 'Error: Arrays are not of the same length';
+    }
+    let result = [];
+    for(let i = 0; i < array1.length; i++)
+    {
+        result.push(array1[i]);
+        result.push(array2[i]);
+    }
+    return result;
+}
+
+console.log(weaveArrays([1,2,3], ['one', 'two', 'three']));
+console.log(weaveArrays([1,2], ['one', 'two', 'three']));
+
+function summarizeOrders(orders)
+{
+    let summary = {};
+    summary["orders"] = [];
+    let totalTip = 0, totalRating = 0; 
+    for(let order of orders)
+    {
+        let orderDetails = order.split(',');
+        summary["orders"].push({
+            "restaurant": orderDetails[0],
+            "cost": orderDetails[1],
+            "tip": parseInt(orderDetails[2]),
+            "rating": parseFloat(orderDetails[3])
+        });
+        //Gradually calculate total tip and rating
+        totalTip += parseInt(orderDetails[2]);
+        totalRating += parseFloat(orderDetails[3]);
+    }
+    //Calculate average tip and rating and add to summary
+    summary["tipTotal"] = totalTip;
+    summary["averageRating"] = totalRating/orders.length;
+    return summary;
+
+}
+
+console.log(JSON.stringify(summarizeOrders([
+  "Panda Express,15.50,3,5",
+  "Chipotle,12.75,2,5",
+  "Subway,,1,4",
+  "Shake Shack,18,4,4"
+])));
